349656b49efd0e2963470771357486f00a1ffe93
Nget-Darapich <ngetdarapich23@gmail.com>
2025-05-21 10:57:19 +0700

    new

---
+++ test/TP08/TP08.js
@@ -0,0 +1,33 @@
+//ex1
+// function greetingRobot(name, color) {
+//     alert("Hello " + name + "," + "I see you like " + color + ". I'm a robot, I like that color too!")
+// }
+// let name = prompt("Enter name: ");
+// let color = prompt("Enter color: ");
+// greetingRobot(name, color);
+
+//ex2
+// const generateCodeName = function(name){
+//     console.log("Agent " + name.toUpperCase())
+// }
+// generateCodeName("sophea");
+
+//ex3
+// const getGrade = (score) =>{
+//     if(score>=90){
+//         console.log("A");
+//     }
+//     else if(score>=80 && score<90){
+//         console.log("B");
+//     }
+//     else if(score>=70 && score<80){
+//         console.log("C");
+//     }
+//     else if(score>=60 && score<70){
+//         console.log("D");
+//     }
+//     else if(score<60){
+//         console.log("F");
+//     }
+// }
+// getGrade(59);
