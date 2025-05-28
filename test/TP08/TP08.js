9a9aa567f51f883fc832761df282ff366295aaca
Nget-Darapich <ngetdarapich23@gmail.com>
2025-05-29 00:07:40 +0700

    new

--- test/TP08/TP08.js
+++ test/TP08/TP08.js
@@ -1,18 +1,20 @@
-//ex1
+//ex1 Robot Greeter
 // function greetingRobot(name, color) {
-//     alert("Hello " + name + "," + "I see you like " + color + ". I'm a robot, I like that color too!")
+//     alert("Hello " + name + "," + "I see you like " + color + ". I'm a robot, but I like that color too!")
 // }
-// let name = prompt("Enter name: ");
-// let color = prompt("Enter color: ");
+// let name = prompt("Enter your name: ");
+// let color = prompt("Enter your favorite color: ");
 // greetingRobot(name, color);
 
-//ex2
+
+//ex2 Secret Code Name Generator
 // const generateCodeName = function(name){
 //     console.log("Agent " + name.toUpperCase())
 // }
 // generateCodeName("sophea");
 
-//ex3
+
+//ex3 Grade Evaluator
 // const getGrade = (score) =>{
 //     if(score>=90){
 //         console.log("A");
@@ -30,4 +32,53 @@
 //         console.log("F");
 //     }
 // }
-// getGrade(59);
+// getGrade(95); // "A"
+// getGrade(82); // "B"
+// getGrade(68); // "D"
+// getGrade(45); // "F"
+
+
+//ex4 Spell Out Your Name
+// function spellOutName(name) {
+//     for (i = 0; i < name.length; i++) {
+//         console.log(name[i]);
+//     }
+// }
+// spellOutName("Anna");
+
+
+//ex5 Music Playlist Manager
+// function addSong(playlist, song) {
+//     playlist.push(song);
+//     console.log('["' + playlist.join('", "') + '"]');
+// }
+// addSong(["Shape of You", "Blinding Lights"], "Bad Guy");
+
+//ex6 Alien Translator
+// const toAlienLanguage = (sentence) => {
+//     if (/[ieaou]/i.test(sentence)) {
+//         let alienSentence = sentence.replace(/[ieaou]/g, '*');
+//         console.log(alienSentence);
+//     }
+// }
+// toAlienLanguage("hello world");
+// toAlienLanguage("javascript is fun");
+
+
+//7 Password Strength Checker
+// const checkPassword = function(password) {
+//     const hasNumber = /\d/;
+//     const hasSpecial = /[!@#]/;
+//     if (
+//         password.length >= 8 &&
+//         hasNumber.test(password) &&
+//         hasSpecial.test(password)
+//     ) {
+//         console.log("Your password is Strong");
+//     } else {
+//         console.log("Your password is Weak");
+//     }
+// };
+// checkPassword("hello123");
+// checkPassword("Hello@123");
+// checkPassword("short1!");
